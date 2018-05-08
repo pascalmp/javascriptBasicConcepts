96bbb9320cab2cdc4a64c076a9416720fdaf5906
pascalmp <pascalmpaw@gmail.com>
2018-05-08 00:32:06 -0400

    Data types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-let myName = 'Pascal';
-myName = 'Oscar';
-console.log(myName);
+let myFirstName = 'Pascal';
+let myLastName = 'Mpawenayo';
+console.log(`${myFirstName} ${myLastName}`);//concantenate strings
+
