1e5c97f9436e94f80f7357503a6b7544295ff824
pascalmp <pascalmpaw@gmail.com>
2018-05-09 23:39:55 -0400

    parseInt/Float a string

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ let example = 7.77;
 // example = 7;
 //console.log(parseInt(example));
 //console.log(parseFloat(example));
-console.log(example.toFixed(9));
+console.log(example.toFixed(0));
+
+let ex1 = parseInt("Hello 33 world 22");
+let ex2 = parseFloat('23 pascal 34');
+let ex3 = 55.3333.toFixed(0);
+let ex4 = 200.0.toFixed(2);
+
+console.log(ex1);
+console.log(ex2);
+console.log(ex3);
+console.log(ex4);
