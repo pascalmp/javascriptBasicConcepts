f2f21496dfb69f5db5198999afd0e25fb9a77cdf
pascalmp <pascalmpaw@gmail.com>
2018-05-10 00:06:51 -0400

    Boolean

--- index.js
+++ index.js
@@ -7,18 +7,44 @@ let fullName = `${myFirstName} ${myLastName}`;
 console.log(fullName);
 
 //NUMBERS
-let example = 7.77;
+//let example = 7.77;
 // example = 7;
 //console.log(parseInt(example));
 //console.log(parseFloat(example));
-console.log(example.toFixed(0));
+// console.log(example.toFixed(0));
 
-let ex1 = parseInt("Hello 33 world 22");
-let ex2 = parseFloat('23 pascal 34');
-let ex3 = 55.3333.toFixed(0);
-let ex4 = 200.0.toFixed(2);
+// let ex1 = parseInt("Hello 33 world 22");
+// let ex2 = parseFloat('23 pascal 34');
+// let ex3 = 55.3333.toFixed(0);
+// let ex4 = 200.0.toFixed(2);
 
-console.log(typeof ex1);//Number
-console.log(typeof ex2);//Number
-console.log(typeof ex3);//String
-console.log(typeof ex4);//String
+// console.log(typeof ex1);//Number
+// console.log(typeof ex2);//Number
+// console.log(typeof ex3);//String
+// console.log(typeof ex4);//String
+
+//BOOLEAN
+// let ex11 = 666;
+
+// console.log(Boolean(ex11) );
+
+let ex12 = false;//false
+let ex13 = true;//true
+let ex14 = null;//false
+let ex15 = NaN;//false
+let ex16 = '';//false ''
+let ex17 = ' ';//true '  '
+let ex18 = undefined;//false
+let ex19 = -3;//true
+let ex20 = 0;//false
+
+console.log(Boolean(ex12));
+console.log(Boolean(ex13));
+console.log(Boolean(ex14));
+console.log(Boolean(ex15));
+console.log(Boolean(ex16));
+console.log(Boolean(ex17));
+console.log(Boolean(ex18));
+console.log(Boolean(ex19));
+console.log(Boolean(ex20));
+
