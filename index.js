7d942e001b0bca9bba00f2a9d4a3d8e8454d0fae
pascalmp <pascalmpaw@gmail.com>
2018-05-14 23:50:22 -0400

    comment

--- index.js
+++ index.js
@@ -28,25 +28,25 @@ console.log(fullName);
 
 // console.log(Boolean(ex11) );
 
-let ex12 = false;//false
-let ex13 = true;//true
-let ex14 = null;//false
-let ex15 = NaN;//false
-let ex16 = '';//false ''
-let ex17 = ' ';//true '  '
-let ex18 = undefined;//false
-let ex19 = -3;//true
-let ex20 = 0;//false
+// let ex12 = false;//false
+// let ex13 = true;//true
+// let ex14 = null;//false
+// let ex15 = NaN;//false
+// let ex16 = '';//false ''
+// let ex17 = ' ';//true '  '
+// let ex18 = undefined;//false
+// let ex19 = -3;//true
+// let ex20 = 0;//false
 
-console.log(Boolean(ex12));
-console.log(Boolean(ex13));
-console.log(Boolean(ex14));
-console.log(Boolean(ex15));
-console.log(Boolean(ex16));
-console.log(Boolean(ex17));
-console.log(Boolean(ex18));
-console.log(Boolean(ex19));
-console.log(Boolean(ex20));
+// console.log(Boolean(ex12));
+// console.log(Boolean(ex13));
+// console.log(Boolean(ex14));
+// console.log(Boolean(ex15));
+// console.log(Boolean(ex16));
+// console.log(Boolean(ex17));
+// console.log(Boolean(ex18));
+// console.log(Boolean(ex19));
+// console.log(Boolean(ex20));
 
 //Arrays
 
